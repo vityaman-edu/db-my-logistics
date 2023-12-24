53b58b0d6000bdb49a99332c42754223f2bc72c3
Victor Smirnov <vityaman.dev@yandex.ru>
2023-12-24 15:46:28 +0300

    #5 [db] refactored storage_balance

--- src/sql/logic/storage/storage_balance.sql
+++ src/sql/logic/storage/storage_balance.sql
@@ -1,47 +1,40 @@
 CREATE VIEW storage_transfer_income AS
   SELECT
-    storage.id AS storage_id, 
+    transfer.dst_storage_id    AS storage_id, 
     transfer_atom.item_kind_id AS item_kind_id, 
-    SUM(amount) AS amount
+    SUM(amount)                AS amount
   FROM transfer
-  JOIN storage ON storage.id = transfer.dst_storage_id
   JOIN transfer_atom ON transfer_atom.transfer_id = transfer.id
-  GROUP BY storage.id, transfer_atom.item_kind_id
-  ORDER BY storage.id;
+  GROUP BY transfer.dst_storage_id, transfer_atom.item_kind_id;
 
 CREATE VIEW storage_supply_income AS
   SELECT 
-    supply.dst_storage_id as storage_id,
-    supply_atom.item_kind_id as item_kind_id,
-    SUM(amount) AS amount
+    supply.dst_storage_id    AS storage_id,
+    supply_atom.item_kind_id AS item_kind_id,
+    SUM(amount)              AS amount
   FROM supply
   JOIN supply_atom ON supply_atom.supply_id = supply.id
-  GROUP BY supply.dst_storage_id, supply_atom.item_kind_id
-  ORDER BY supply.dst_storage_id;
+  GROUP BY supply.dst_storage_id, supply_atom.item_kind_id;
 
 CREATE VIEW storage_income AS
   SELECT storage_id, item_kind_id, SUM(amount) AS amount
   FROM (
     (SELECT * FROM storage_transfer_income) UNION 
     (SELECT * FROM storage_supply_income))
-  GROUP BY storage_id, item_kind_id
-  ORDER BY storage_id;
+  GROUP BY storage_id, item_kind_id;
 
 CREATE VIEW storage_outcome AS 
   SELECT
-      storage.id AS storage_id, 
+      transfer.src_storage_id    AS storage_id, 
       transfer_atom.item_kind_id AS item_kind_id, 
-      -SUM(amount) AS amount
+      -SUM(amount)               AS amount
   FROM transfer
-  JOIN storage ON storage.id = transfer.src_storage_id
   JOIN transfer_atom ON transfer_atom.transfer_id = transfer.id
-  GROUP BY storage.id, transfer_atom.item_kind_id
-  ORDER BY storage.id;
+  GROUP BY transfer.src_storage_id, transfer_atom.item_kind_id;
 
 CREATE VIEW storage_balance AS 
   SELECT storage_id, item_kind_id, SUM(amount) AS amount
   FROM (
     (SELECT * FROM storage_income) UNION 
     (SELECT * FROM storage_outcome))
-  GROUP BY storage_id, item_kind_id
-  ORDER BY storage_id;
+  GROUP BY storage_id, item_kind_id;
--- src/sql/util/02-test.sql
+++ src/sql/util/02-test.sql
@@ -1,19 +1,23 @@
 SELECT storage.name, item_kind.name, capacity
 FROM storage
 JOIN storage_capacity ON storage.id = storage_capacity.storage_id
-JOIN item_kind ON storage_capacity.item_kind_id = item_kind.id;
+JOIN item_kind ON storage_capacity.item_kind_id = item_kind.id
+ORDER BY storage.name;
 
 SELECT storage.name, item_kind.name, amount
 FROM storage
 JOIN storage_income ON storage.id = storage_income.storage_id
-JOIN item_kind ON storage_income.item_kind_id = item_kind.id;
+JOIN item_kind ON storage_income.item_kind_id = item_kind.id
+ORDER BY storage.name;
 
 SELECT storage.name, item_kind.name, amount
 FROM storage
 JOIN storage_outcome ON storage.id = storage_outcome.storage_id
-JOIN item_kind ON storage_outcome.item_kind_id = item_kind.id;
+JOIN item_kind ON storage_outcome.item_kind_id = item_kind.id
+ORDER BY storage.name;
 
 SELECT storage.name, item_kind.name, amount
 FROM storage
 JOIN storage_balance ON storage.id = storage_balance.storage_id
-JOIN item_kind ON storage_balance.item_kind_id = item_kind.id;
+JOIN item_kind ON storage_balance.item_kind_id = item_kind.id
+ORDER BY storage.name;
