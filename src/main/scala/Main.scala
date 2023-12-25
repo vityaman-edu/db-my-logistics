63169ac6d1553a12f7746b4327e3f8f68d7e3a09
Victor Smirnov <vityaman.dev@yandex.ru>
2023-12-25 16:16:50 +0300

    #7 [back] setup zio cli hello world

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,10 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import zio._
+import zio.Console
 
-def msg = "I was compiled by Scala 3. :)"
+object MyApp extends ZIOAppDefault {
+  def run = for {
+      _    <- Console.printLine("Hello! What is your name?")
+      name <- Console.readLine
+      _    <- Console.printLine(s"Hello, ${name}, welcome to ZIO!")
+    } yield ()
+}
