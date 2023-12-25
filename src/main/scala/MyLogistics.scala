c927333438e714a9fd57e2fb4f3e7c52199d6042
Victor Smirnov <vityaman.dev@yandex.ru>
2023-12-25 17:07:03 +0300

    #7 [back] tried to add logging

--- src/main/scala/MyLogistics.scala
+++ src/main/scala/MyLogistics.scala
@@ -1,8 +1,13 @@
 import zio.http.Server
-import zio.ZIOAppDefault
+import zio.*
 import api.http.HttpApi
 
 object MyLogistics extends ZIOAppDefault {
-  override val run =
-    Server.serve(HttpApi.app).provide(Server.default)
+  override def run = for {
+    _ <- ZIO.log("Starting service MyLogistics...")
+    server <- Server
+      .serve(HttpApi.app)
+      .provide(Server.default)
+      .onInterrupt(ZIO.log("Service was shut down."))
+  } yield server
 }
