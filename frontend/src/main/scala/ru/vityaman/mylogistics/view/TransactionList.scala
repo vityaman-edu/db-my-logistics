41d5be8496743d0ee3b593d26f2c966caf864277
Victor Smirnov <vityaman.dev@yandex.ru>
2024-01-19 16:59:18 +0300

    #18 [front] added transaction filters

--- frontend/src/main/scala/ru/vityaman/mylogistics/view/TransactionList.scala
+++ frontend/src/main/scala/ru/vityaman/mylogistics/view/TransactionList.scala
@@ -5,11 +5,38 @@ import ru.vityaman.mylogistics.model.Transaction
 import com.raquo.laminar.api.L._
 
 object TransactionList {
-  def apply(transactions: Signal[List[Transaction]]): HtmlElement =
-    Table(
-      Seq("Time", "Storage", "Item", "Amount", "Unit"),
-      tbody(children <-- transactions.map(_.map(render)))
+  def apply(transactions: Signal[List[Transaction]]): HtmlElement = {
+    val storageContains: Var[String] = Var("")
+    val itemKindContains: Var[String] = Var("")
+
+    val filtered = transactions.signal
+      .combineWith(storageContains.signal)
+      .mapN((list, part) => list.filter(_.storage.name.contains(part)))
+      .combineWith(itemKindContains.signal)
+      .mapN((list, part) => list.filter(_.itemKind.name.contains(part)))
+
+    div(
+      div(
+        display.flex,
+        flexDirection.row,
+        justifyContent.center,
+        div(
+          margin.percent(1),
+          a("Storage: "),
+          input(onInput.mapToValue --> storageContains)
+        ),
+        div(
+          margin.percent(1),
+          a("Item Kind: "),
+          input(onInput.mapToValue --> itemKindContains)
+        )
+      ),
+      Table(
+        Seq("Time", "Storage", "Item", "Amount", "Unit"),
+        tbody(children <-- filtered.map(_.map(render)))
+      )
     )
+  }
 
   private def render(transaction: Transaction): HtmlElement =
     tr(
